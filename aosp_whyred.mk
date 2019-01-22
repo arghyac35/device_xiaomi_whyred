a727f0106ffcb576c55652d66cb451ff7704db65
Arghya Chanda <arghyac35@gmail.com>
2019-01-22 13:38:03 +0530

    whyred: aex-fy
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -22,19 +22,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Set Boot Animination Resolution
+TARGET_BOOT_ANIMATION_RES := 2140
+
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
-
-# Inherit some common PixelExperience stuff.
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Redmi Note 5
+PRODUCT_MODEL := Redmi Note 5 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
