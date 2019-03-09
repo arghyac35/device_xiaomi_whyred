a36690db8c036517cc823afe87d4adfe45b59ca7
Shubham Singh <coolsks94@gmail.com>
2019-03-09 12:03:41 +0100

    whyred: include gapps
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -47,3 +47,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := "xiaomi/whyred/whyred:9/PKQ1.180904.001/V10.3.1.0.PEIMIXM:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
+
+# Use Gapps
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
