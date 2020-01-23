kapiljhajhria/android_device_xiaomi_whyred-3

whyred: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_whyred.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_whyred-user \
+    lineage_whyred-userdebug \
+    lineage_whyred-eng

arghyac35/device_xiaomi_whyred

Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_whyred.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_whyred-user \
+    aosp_whyred-userdebug \
+    aosp_whyred-eng